66d171ced8f817543b571badcc6def2ed7153d8d
ValentinBezuglenko <valentinbezuglenko715@gmail.com>
2025-10-06 09:04:30 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,35 +1,26 @@
-import socket
+import asyncio
+import websockets
 import wave
 
-HOST = '0.0.0.0'
-PORT = 5005
 SAMPLE_RATE = 16000
 CHANNELS = 1
 SAMPLE_WIDTH = 2  # 16-bit
 
-filename = "recording_from_esp32.wav"
+async def handler(websocket, path):
+    filename = "recording_stream.wav"
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(SAMPLE_WIDTH)
+    wf.setframerate(SAMPLE_RATE)
 
-# TCP сервер
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((HOST, PORT))
-sock.listen(1)
-print("Waiting for ESP32 connection...")
-conn, addr = sock.accept()
-print(f"Connected: {addr}")
+    try:
+        async for message in websocket:
+            wf.writeframes(message)
+    finally:
+        wf.close()
+        print("Saved WAV:", filename)
 
-# WAV файл
-wf = wave.open(filename, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(SAMPLE_WIDTH)
-wf.setframerate(SAMPLE_RATE)
+start_server = websockets.serve(handler, '0.0.0.0', 8765)  # порт 8765 для теста
 
-try:
-    while True:
-        data = conn.recv(3200*2)  # 3200 байт = 1600 сэмплов
-        if not data:
-            break
-        wf.writeframes(data)
-finally:
-    conn.close()
-    wf.close()
-    print("Recording saved:", filename)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
